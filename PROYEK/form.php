d3801202136a4a492e05b626d2f86980ce771071
Raman <91239554+ramansssss@users.noreply.github.com>
2022-06-02 10:04:30 +0700

    Add files via upload

---
+++ PROYEK/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Form Login</title>
+</head>
+<body>
+ <form class="" action="" method="">
+ <label for="">Email</label>
+ <input type="text" name="email" autocomplete="off"><br>
+ <label for="">Password</label>
+ <input type="text" name="password" autocomplete="off"><br>
+ </form>
+</body>
+</html>
